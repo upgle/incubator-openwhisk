a0c2166eee7aa1d97d369936f1004b373194aa86
甯尤刚[PaaS Platform] <ningyougang@navercorp.com>
2018-01-15 10:47:12 +0800

    Make invoker zerodowntime deployment (#222)
    Before redeploy invoker, should send unhealthy invoker message to
    controller to enable the invoker to offline(10 seconds later),
    On the other hand, give proper time to execute non-finished actions.
    
    Another thing, sleep enough time to wait redeployed invoker's status is up.

--- core/invoker/src/main/scala/whisk/core/invoker/Invoker.scala
+++ core/invoker/src/main/scala/whisk/core/invoker/Invoker.scala
@@ -27,9 +27,9 @@ import whisk.common.Https.HttpsConfig
 import whisk.common._
 import whisk.core.WhiskConfig
 import whisk.core.WhiskConfig._
-import whisk.core.connector.{MessagingProvider, PingMessage}
+import whisk.core.connector.{MessageProducer, MessagingProvider, PingMessage}
 import whisk.core.entity.{ExecManifest, InvokerInstanceId}
-import whisk.http.{BasicHttpService, BasicRasService}
+import whisk.http.BasicHttpService
 import whisk.spi.SpiLoader
 import whisk.utils.ExecutionContextFactory
 
@@ -42,6 +42,10 @@ case class CmdLineArgs(uniqueName: Option[String] = None, id: Option[Int] = None
 object Invoker {
 
   protected val protocol = loadConfigOrThrow[String]("whisk.invoker.protocol")
+
+  var messageProducer: Option[MessageProducer] = None
+
+  var currentInstance: Option[InvokerInstanceId] = None
 
   /**
    * An object which records the environment variables required for this component to run.
@@ -143,6 +147,7 @@ object Invoker {
     val topicBaseName = "invoker"
     val topicName = topicBaseName + assignedInvokerId
     val invokerInstance = InvokerInstanceId(assignedInvokerId, cmdLineArgs.uniqueName, cmdLineArgs.displayedName)
+    currentInstance = Some(invokerInstance)
     val msgProvider = SpiLoader.get[MessagingProvider]
     if (msgProvider.ensureTopic(config, topic = topicName, topicConfig = topicBaseName).isFailure) {
       abort(s"failure during msgProvider.ensureTopic for topic $topicName")
@@ -153,18 +158,23 @@ object Invoker {
     } catch {
       case e: Exception => abort(s"Failed to initialize reactive invoker: ${e.getMessage}")
     }
-
+    // HealthProducer and completedProducer can't use same instance, because if some action need to execute long time
+    // and then need to redeploy, should ensure that the long time action's execute result can return back success
+    // through completedProducer
+    val healthProducer = msgProvider.getProducer(config)
     Scheduler.scheduleWaitAtMost(1.seconds)(() => {
-      producer.send("health", PingMessage(invokerInstance)).andThen {
+      healthProducer.send("health", PingMessage(invokerInstance)).andThen {
         case Failure(t) => logger.error(this, s"failed to ping the controller: $t")
       }
     })
+    messageProducer = Some(healthProducer)
 
     val port = config.servicePort.toInt
+
     val httpsConfig =
       if (Invoker.protocol == "https") Some(loadConfigOrThrow[HttpsConfig]("whisk.invoker.https")) else None
 
-    BasicHttpService.startHttpService(new BasicRasService {}.route, port, httpsConfig)(
+    BasicHttpService.startHttpService(new InvokerServer().route, port, httpsConfig)(
       actorSystem,
       ActorMaterializer.create(actorSystem))
   }
