83afc9d43675aa75835ba78af1e53ac591cd4f23
甯尤刚[PaaS Platform] <ningyougang@navercorp.com>
2018-01-15 11:10:05 +0800

    Adjust logging (#230)
    - Fix bug, when redeploy invokers, the invoker will delete the original
      log file and create a new one, so should recreate the hardlink file.
    - Add logrotate to save storage space.
    - Add nginx error log and send it to nelo.

--- ansible/roles/neloagent/files/create-hardlink.sh
+++ ansible/roles/neloagent/files/create-hardlink.sh
@@ -12,6 +12,13 @@ function deal()
                 # create hard link
                 if [ ! -f "${WSK_TOP_LOG_DIR}/lambdalog/${file}" ]; then
                     ln ${path} ${WSK_TOP_LOG_DIR}/lambdalog/${file}
+                else
+                    # Fix bug, because when redeploy invoker, will create a new log file.
+                    # So compare the inode, if not equal, delete and create a new hardlink file.
+                    if [ `stat -c %i ${path}` != `stat -c %i ${WSK_TOP_LOG_DIR}/lambdalog/${file}` ]; then
+                        rm -f ${WSK_TOP_LOG_DIR}/lambdalog/${file}
+                        ln ${path} ${WSK_TOP_LOG_DIR}/lambdalog/${file}
+                    fi
                 fi
             fi
         fi
