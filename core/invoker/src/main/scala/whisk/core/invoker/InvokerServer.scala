15531861c6d7687bd6575cd4aa19629415f1f4c6
甯尤刚[PaaS Platform] <ningyougang@navercorp.com>
2018-01-15 10:47:12 +0800

    Make invoker zerodowntime deployment (#222)
    Before redeploy invoker, should send unhealth invoker message to
    controller to enable the invoker to offline(10 seconds later),
    On the other hand, give proper time to execute non-finished actions.
    
    Another thing, sleep enough time to wait redeployed invoker's status is up.

--- core/invoker/src/main/scala/whisk/core/invoker/InvokerServer.scala
+++ core/invoker/src/main/scala/whisk/core/invoker/InvokerServer.scala
@@ -17,12 +17,60 @@
 
 package whisk.core.invoker
 
+import akka.actor.ActorSystem
+import akka.http.scaladsl.server.Route
+import whisk.common.{Logging, Scheduler, TransactionId}
+import whisk.core.WhiskConfig
+import whisk.core.connector.{MessagingProvider, PingMessage}
+import whisk.core.entity.InstanceId
 import whisk.http.BasicRasService
+import whisk.spi.SpiLoader
+
+import scala.concurrent.ExecutionContext
+import scala.concurrent.duration._
+import scala.util.Failure
 
 /**
  * Implements web server to handle certain REST API calls.
  * Currently provides a health ping route, only.
  */
-class InvokerServer() extends BasicRasService {
+class InvokerServer(implicit val ec: ExecutionContext, implicit val actorSystem: ActorSystem, implicit val logger: Logging) extends BasicRasService {
+
   override val instanceOrdinal = 1
+
+  override def routes(implicit transid: TransactionId): Route = {
+    super.routes ~ {
+      (path("disable") & get) {
+        Invoker.messageProducer match {
+          case Some(producer) => {
+            // Through negative number of invoker to pass unhealth message
+            producer.send("health", PingMessage(InstanceId(-1 - Invoker.currentInstance.get.instance)))
+            producer.close();
+            Invoker.messageProducer=None;
+            complete("Success disable invoker");
+          }
+          case None => complete("Can't disable invoker again")
+        }
+      }
+    } ~ {
+      (path("enable") & get) {
+        Invoker.messageProducer match {
+          case Some(_) => {
+            complete("Can't enable invoker again");
+          }
+          case None =>{
+            val msgProvider = SpiLoader.get[MessagingProvider]
+            val healthProducer = msgProvider.getProducer(new WhiskConfig(Invoker.requiredProperties), ec)
+            Scheduler.scheduleWaitAtMost(1.seconds)(() => {
+              healthProducer.send("health", PingMessage(InstanceId(Invoker.currentInstance.get.instance))).andThen {
+                case Failure(t) => logger.error(this, s"enable invoker, failed to ping the controller: $t")
+              }
+            })
+            Invoker.messageProducer = Some(healthProducer)
+            complete("Success enable invoker");
+          }
+        }
+      }
+    }
+  }
 }
