0d95df6b0ffc14d73a73a3882e84e899d910338c
甯尤刚[PaaS Platform] <ning.yougang@navercorp.com>
2018-04-25 10:06:33 +0800

    Add auth when disable invoker (#315)
    It is necessary to add auth when /disable invoker.
    
    Resolved: paas/lambda#313

--- core/invoker/src/main/scala/whisk/core/invoker/InvokerServer.scala
+++ core/invoker/src/main/scala/whisk/core/invoker/InvokerServer.scala
@@ -1,6 +1,8 @@
 package whisk.core.invoker
 
 import akka.actor.ActorSystem
+import akka.http.scaladsl.model.StatusCodes
+import akka.http.scaladsl.model.headers.BasicHttpCredentials
 import akka.http.scaladsl.server.Route
 import whisk.common.{Logging, Scheduler, TransactionId}
 import whisk.core.WhiskConfig
@@ -11,7 +13,7 @@ import whisk.spi.SpiLoader
 
 import scala.concurrent.ExecutionContext
 import scala.concurrent.duration._
-import scala.util.Failure
+import scala.util.{Failure, Success}
 
 /**
   * Implements web server to handle certain REST API calls.
@@ -19,37 +21,61 @@ import scala.util.Failure
   */
 class InvokerServer(implicit val ec: ExecutionContext, implicit val actorSystem: ActorSystem, implicit val logger: Logging) extends BasicRasService {
 
+  val invokerUsername = {val source = scala.io.Source.fromFile("/conf/invokerauth.username");try source.mkString.replaceAll("\r|\n", "") finally source.close()}
+  val invokerPassword= {val source = scala.io.Source.fromFile("/conf/invokerauth.password");try source.mkString.replaceAll("\r|\n", "") finally source.close()}
+
   override def routes(implicit transid: TransactionId): Route = {
     super.routes ~ {
       (path("disable") & get) {
-        Invoker.messageProducer match {
-          case Some(producer) => {
-            // Through negative number of invoker to pass unhealth message
-            producer.send("health", PingMessage(InvokerInstanceId(-1 - Invoker.currentInstance.get.instance, Invoker.currentInstance.get.name)))
-            producer.close();
-            Invoker.messageProducer = None;
-            complete("Success disable invoker");
-          }
-          case None => complete("Can't disable invoker again")
+        extractCredentials {
+          case Some(BasicHttpCredentials(username, password)) =>
+            if(username == invokerUsername && password == invokerPassword){
+              Invoker.messageProducer match {
+                case Some(producer) => {
+                  // Through negative number of invoker to pass unhealthy message
+                  producer.send("health", PingMessage(InvokerInstanceId(-1 - Invoker.currentInstance.get.instance, Invoker.currentInstance.get.uniqueName)), 3).andThen {
+                    case Success(_) =>
+                      producer.close();
+                      logger.info(this, "send unhealthy message successfully")
+                    case Failure(_) =>
+                      logger.info(this, "failed to send unhealthy message")
+                  }
+                  Invoker.messageProducer=None;
+                  complete("Success disable invoker");
+                }
+                case None => complete("Can't disable invoker again")
+              }
+            }else{
+              complete("username or password is wrong")
+            }
+          case _ => complete(StatusCodes.Unauthorized)
         }
       }
     } ~ {
       (path("enable") & get) {
-        Invoker.messageProducer match {
-          case Some(_) => {
-            complete("Can't enable invoker again");
-          }
-          case None => {
-            val msgProvider = SpiLoader.get[MessagingProvider]
-            val healthProducer = msgProvider.getProducer(new WhiskConfig(Invoker.requiredProperties))
-            Scheduler.scheduleWaitAtMost(1.seconds)(() => {
-              healthProducer.send("health", PingMessage(InvokerInstanceId(Invoker.currentInstance.get.instance, Invoker.currentInstance.get.name))).andThen {
-                case Failure(t) => logger.error(this, s"enable invoker, failed to ping the controller: $t")
+        extractCredentials {
+          case Some(BasicHttpCredentials(username, password)) =>
+            if(username == invokerUsername && password == invokerPassword){
+              Invoker.messageProducer match {
+                case Some(_) => {
+                  complete("Can't enable invoker again");
+                }
+                case None =>{
+                  val msgProvider = SpiLoader.get[MessagingProvider]
+                  val healthProducer = msgProvider.getProducer(new WhiskConfig(Invoker.requiredProperties))
+                  Scheduler.scheduleWaitAtMost(1.seconds)(() => {
+                    healthProducer.send("health", PingMessage(InvokerInstanceId(Invoker.currentInstance.get.instance, Invoker.currentInstance.get.uniqueName))).andThen {
+                      case Failure(t) => logger.error(this, s"enable invoker, failed to ping the controller: $t")
+                    }
+                  })
+                  Invoker.messageProducer = Some(healthProducer)
+                  complete("Success enable invoker");
+                }
               }
-            })
-            Invoker.messageProducer = Some(healthProducer)
-            complete("Success enable invoker");
-          }
+            }else{
+              complete("username or password is wrong")
+            }
+          case _ => complete(StatusCodes.Unauthorized)
         }
       }
     }
